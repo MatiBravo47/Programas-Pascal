6c334e076c49b7cefd121581605d90288d51a78f
MatiBravo <Matibravoneron@gmail.com>
2022-05-15 14:36:39 -0300

    Agregue un ejercicio

---
+++ Cap7Eje2.pas
@@ -0,0 +1,9 @@
+program Cap7Eje2;
+Uses crt;
+
+
+begin
+  clrscr;
+  readln;
+end.
+
---
+++ Cap7Eje4.pas
@@ -0,0 +1,41 @@
+program Cap7Eje4;
+uses crt;
+Type
+  TR = Record
+    Club:string;
+    PJ:byte;
+  end;
+TV = array [1..10] of TR; //Vector que guarda todos los datos
+
+Procedure LeeArch(Var Equipos:TV);
+Var
+  arch:text;
+  i,partidos:byte;
+  nombre: string[6];
+Begin
+ASSIGN(Arch,'Ejercicio74.txt');RESET(Arch);
+Readln(Arch,N);//Lee cantidad de equipos
+For i:=1 to N do
+  begin
+  perdio:=false;
+  puntaje:=0;
+    With V[i] do
+    Read(Arch,Nombre,PJ);
+    For j:=1 to PJ do
+      begin
+      Read(Arch,Res);
+      if res = 'G' then
+        Puntaje:=Puntaje + 3;
+      if res = 'E' then
+        Puntaje := Puntaje + 1;
+      if res = 'P' then
+        Perdio:=true; //clequear esto
+       end;
+  Readln(Arch);
+  end;
+CLOSE(Arch);
+end;
+
+begin
+end.
+
