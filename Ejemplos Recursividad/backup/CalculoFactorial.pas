b19fc5c68d33e57eaf9ca124088e547d298761b6
MatiBravo <Matibravoneron@gmail.com>
2022-05-15 17:56:08 -0300

    ejemplo de recursividad

---
+++ Ejemplos Recursividad/backup/CalculoFactorial.pas
@@ -0,0 +1,20 @@
+program CalculoFactorial;
+uses crt;
+Function FacRec (N:word):word;
+Begin
+  If (n=0) or (n=1) then
+    facRec := 1
+  else
+    facRec := N * facRec(N-1);
+end;
+//Programa principal
+var
+  n:byte;
+
+begin
+  clrscr;
+  Writeln('Ingrese un numero');Readln(n);
+  Writeln('el factorial de ese numero es: ',facRec(n));
+  Readln;
+end.
+
