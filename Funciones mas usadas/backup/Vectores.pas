db201777f61ee9d996639d97209ab1dfa5a9e95d
MatiBravo <Matibravoneron@gmail.com>
2022-05-18 19:18:47 -0300

    Agregue unas funciones

--- Funciones mas usadas/backup/Vectores.pas
+++ Funciones mas usadas/backup/Vectores.pas
@@ -1,4 +1,5 @@
 program Vectores;
+Uses crt;
 
 type
   St10=String[10];
@@ -7,7 +8,7 @@ type
   TVReal=array['A'..'Z'] of real;
   TV=array[1..12] of byte;
 Const
-  A:TV=(31,28,31,30,31,30,31,31,30,31,30,31);
+  V:TV=(31,28,31,30,31,30,31,31,30,31,30,31);
 
 //Suma todos los elementos del vector
 Function Suma (A:TV;N:integer):integer;
@@ -21,26 +22,52 @@ Begin
 end;
 
 //El minimo del vector
-Function minimo(A:tv;N:integer):Integer;
+Function minimo(V:tv;N:integer):Integer;
 Var
   i,min:integer;
 begin
-  min:=A[1];
+  min:=V[1];
   for i:=2 to N do
-    if A[i]< min then
-      min:=A[i];
+    if V[i]< min then
+      min:=V[i];
   minimo:=Min;
+end;
+//El maximo del vector
+Function Max(V:TV;N:Byte):Integer;
+Var
+  i:Byte;
+  Maximo:Integer;
+Begin
+  Maximo:=V[1];
+  For i:=2 to N do
+    Begin
+    If V[i]>=Maximo then
+    Maximo:=V[i];
+    end;
+Max:=Maximo;
 end;
 
 //Escribe Vector
-Procedure EscVector(V:TV;L:integer);
+Procedure EscVector(V:TV;N:integer);
 var
   i:integer;
 Begin
-  for i:=1 to L do
+  for i:=1 to N do
     Write(V[I]:5)
 end;
-
+//Programa principal
+Var
+  A:TV;
+  N:Byte;
 begin
+clrscr;
+Writeln('Ingrese tamanio vector');Readln(N);
+Writeln('El vector es ');
+EscVector(V,N);
+Writeln();
+Writeln('La suma del vector es ',Suma(V,N));
+Writeln('El minimo del vector es ',Minimo(v,n));
+Writeln('El maximo del vector es ', Max(v,n));
+Readln;
 end.
 
