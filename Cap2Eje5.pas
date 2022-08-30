46204c0e25ff80ddf107b9f9c4adb4e17dc6c85b
MatiBravo47 <Matibravoneron@gmail.com>
2022-08-30 09:53:28 -0300

    add explanation, improve style

--- Cap2Eje5.pas
+++ Cap2Eje5.pas
@@ -4,20 +4,21 @@ Var
 A,B,C:Integer;
 begin
 clrscr;
+//permutacion sin repeticion de 3 elementos  => 3!opcion = 6 opciones
 Writeln('Ingrese 3 numeros');Readln(A,B,C);
-If A>B then
+If (A > B) then
   begin
-  if A>C then
-  writeln('El mayor numero es el',A)
-  else
-  Writeln('El mayor numero es el',C)
+  if (A > C) then // BCA O CBA
+    writeln('El mayor numero es el',A)
+  else //  BAC
+    Writeln('El mayor numero es el',C)
   end
 else
   begin
-  if B>C then
-  writeln('El mayor numero es el ',B)
-  else
-  writeln('El mayor numero es el ',C)
+  if (B > C) then //ACB
+    writeln('El mayor numero es el ',B)
+  else //BAC O ABC
+    writeln('El mayor numero es el ',C)
   end;
 Readln;
 end.
