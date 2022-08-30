57c427634b98c85cf83e7d67b6e8f7a49368aa29
MatiBravo47 <Matibravoneron@gmail.com>
2022-08-30 10:13:42 -0300

    add info

--- Cap2Eje6.pas
+++ Cap2Eje6.pas
@@ -4,36 +4,34 @@ Var
 A,B,C:char;
 begin
 clrscr;
+//Permutacion de 3 => 6 posibles soluciones
 Writeln('Ingrese 3 letras');
-Readln(A);
-Readln(B);
-Readln(C);
-A:=upcase(A);
-B:=upcase(B);
-C:=upcase(C);
-If A>B then
-  begin
-  if A>C then
-     begin
-     If C>B then
-     Writeln('El orden ascendente es ' ,B,'',C,'',A)
-     else
-     Writeln('El orden ascendente es ' ,C,'',B,'',A)
-     end
-  Else
-  Writeln('El orden ascendente es ' ,B,'',A,'',C)
-  end
+Readln(A); Readln(B); Readln(C);
+//upcase convierte a mayusculas los caracteres
+A:=upcase(A); B:=upcase(B); C:=upcase(C);
+If (A > B) then
+	 begin
+	 if (A > C) then
+      begin
+      If (C > B) then
+         Writeln('El orden ascendente es ' ,B,'',C,'',A)
+      else
+         Writeln('El orden ascendente es ' ,C,'',B,'',A)
+   end
+else
+		Writeln('El orden ascendente es ' ,B,'',A,'',C)
+		end
 else //si B>A
   begin
-  if B>C then  //Si B es el mayor de todos
+  if (B > C) then  //Si B es el mayor de todos
      begin
-     If A>C then
-     Writeln('El orden ascendente es ' ,C,'',A,'',B)
+     If (A > C) then
+     		Writeln('El orden ascendente es ' ,C,'',A,'',B)
      else
-     Writeln('El orden ascendente es ' ,A,'',C,'',B)
+     		 Writeln('El orden ascendente es ' ,A,'',C,'',B)
      end
-  Else //Si C>B
-  Writeln('El orden ascendente es ' ,A,'',B,'',C)
+  else //Si C>B
+     Writeln('El orden ascendente es ' ,A,'',B,'',C)
   end;
 Readln;
 end.
