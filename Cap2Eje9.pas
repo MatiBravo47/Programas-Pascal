8dc04fdbe1fc1db8d9fbf40d92c8ceb61fd69768
MatiBravo47 <Matibravoneron@gmail.com>
2022-08-30 14:08:31 -0300

    add comments

--- Cap2Eje9.pas
+++ Cap2Eje9.pas
@@ -1,26 +1,36 @@
+//Ej 9) A un vendedor le pagan un sueldo básico de $8000 por mes más
+//una comisión del 5% de las ventas;si superó en ventas un monto X
+//(dato ingresado por teclado) se le agrega un 3%. Por presentismo cobra
+//$1000 más ó 2% de las ventas, lo que resulte mayor. Si faltó más
+//de 2 días se le descuenta un 7% del total.
+//Definir los datos a ingresar. Calcular e informar lo que debe
+//cobrar el vendedor
 program Cap2Eje9;
 uses crt;
+const
+  presentismo = 1000;
 Var
-Monto,X,Total,porce,SueldoB:Real;
-Faltas:Integer;
+  Monto,X,Total,porce,SueldoB:Real;
+	Faltas:Integer;
 
 begin
 clrscr;
-SueldoB:=800;
+SueldoB:=8000;
 Writeln('Ingrese monto obtenido en ventas');Readln(Monto);
-Total:=SueldoB+((5*Monto)/100);
+Total:= SueldoB + (Monto * 1.05);//+ %5 comisiones de las ventas
 Writeln('Ingrese monto para obtener el beneficio');Readln(X);
-If monto>x then
-Total:=Total*1.03;
-Porce:=((2*Monto)/100);
-If Porce > 1000 then
-Total:=Total + porce
-else
-Total:=Total + 1000;
+If (monto > x) then
+	 Total := Total * 1.03; //+ %3 por superar
+	 Porce :=((2 * Monto) / 100);//%2 de monto
 Writeln('Indique cantidad de dias que falto');Readln(Faltas);
-If faltas>2 then
-Total:=Total*0.93;
-Writeln('El vendedor va a cobrar ',Total);
+If (faltas = 0) then
+	 If (Porce > Presentismo ) then
+	 		Total := Total + porce
+	 else
+      Total := Total + Presentismo;
+If (faltas>2) then
+  Total := Total * 0.93; //-7% total
+Writeln('El vendedor va a cobrar $',Total:5:2);
 readln;
 end.
 
