8839c64cf7381cd64bae6396841e051bec6f8c67
MatiBravo47 <Matibravoneron@gmail.com>
2022-08-30 14:55:56 -0300

    add exercise b

--- Cap1Eje6.pas
+++ Cap1Eje6.pas
@@ -7,8 +7,11 @@ begin
   Clrscr;
   Writeln('ingrese un numero entero que contenga mas de 3 cifras');
   Readln (N);
+  //A)
   Num := ((N)div(1000));
   Writeln('quitandole las ultimas 3 cifras: ',Num);
+  //B)
+  Write('En la posicion de las centenas: ',((N div 100) mod 10));
   Readln;
 end.
 
