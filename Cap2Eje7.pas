e9d61556b0a8102db0f94237333ab153dec61fc4
MatiBravo47 <Matibravoneron@gmail.com>
2022-08-30 10:29:21 -0300

    add explanation

--- Cap2Eje7.pas
+++ Cap2Eje7.pas
@@ -1,3 +1,11 @@
+//Ej 7) Un negocio vende camisas, éstas pueden ser de hombre o de mujer.
+//Los talles que fabrican son: S,M, L y X.
+//El precio base unitario es de $1000 para hombres y $1200 para mujer,
+//se debe agregar el IVA del 21%. Hay una promoción en los talles S de hombre
+//y X de mujer con un 5% de descuento. Además, si la compra supera la docena
+//se considera venta mayorista y obtiene un descuento extra del 7% del total.
+//Determinar cuáles son los datos necesarios para calcular el importe
+//a facturar en una venta
 program Cap2Eje7;
 uses crt;
 var
@@ -5,6 +13,13 @@ var
 
 begin
 clrscr;
+//Los datos necesarios para el calcular el importe son:
+//Cantidad de unidades compradas (si son mayores a 12 se considera mayorista)
+//Tipo de talle
+//Precio unitario de hombres y mujeres
+//IVA(21%)
+//Descuento mayorista
+//Descuento talle
 readln;
 end.
 
