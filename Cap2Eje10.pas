47839baef44d483c5c2928d4036e0f24dc7d7149
MatiBravo47 <Matibravoneron@gmail.com>
2022-08-30 14:28:14 -0300

    fix program and[C add comments

--- Cap2Eje10.pas
+++ Cap2Eje10.pas
@@ -1,3 +1,12 @@
+//Ej 10) Una empresa le brinda a sus clientes un servicio de cable
+//con un costo básico de $1350 por mes, el paquete de películas se
+//cobra $200 adicionales y el de deportes $270. Aparte ofrece internet
+//por $800 con 6MB, si se desea 10MB y WIFI cuesta 20% más.
+//La opción de grabar programas tiene un costo adicional
+//de $150 por mes. La promoción es que si se contrata ambos servicios
+//(cable e internet), tiene una bonificación del 25% del total.
+//Ingresar los datos de un cliente, calcular e informar cuánto
+//debe pagar
 program Cap2Eje10;
 uses crt;
 Var
@@ -7,33 +16,35 @@ Opc:Byte;
 
 begin
 clrscr;
-Monto:=1350;
-Writeln('Desea agregar pack peliculas?(S/N)');Readln(Pelis);
-Pelis:=Upcase(Pelis);
-If Pelis='S' then
-Monto:= Monto +200;
-Writeln('Desea agregar pack deportes?(S/N)');Readln(Deportes);
-Deportes:=Upcase(Deportes);
-If deportes='S' then
-Monto:= Monto + 270;
+Monto := 1350; //Costo Basico
+Writeln('Desea agregar pack peliculas?(S/N)'); Readln(Pelis);
+Pelis := Upcase(Pelis);
+If Pelis = 'S' then  //adicionales por pelicula
+	 Monto := Monto + 200;
+Writeln('Desea agregar pack deportes?(S/N)'); Readln(Deportes);
+Deportes := Upcase(Deportes);
+If deportes = 'S' then
+	 Monto := Monto + 270; //adicional por deporte
 Writeln('Desea agregar "Grabar peliculas"(S/N)');Readln(Grabar);
-Grabar:=Upcase(Grabar);
-If Grabar='S' then
-Monto:=Monto +150;
+Grabar := Upcase(Grabar);
+If Grabar = 'S' then
+	 Monto := Monto +150;//adicional por grabar
 Writeln('Desea agregar internet?(S/N)');Readln(Internet);
-Internet:=Upcase(Internet);
+Internet := Upcase(Internet);
 If (internet='S') then
+  begin
   Writeln('elija la cantidad de megas');
   Writeln('1)Internet 6MB ');
   Writeln('2) Internet 10MB + WIFI');
   Readln(Opc);
   Case Opc of
   1: Monto:= Monto + 800;
-  2: Monto:= Monto +(800*1.20);
+  2: Monto:= Monto + ( 800 * 1.20);//+%20 de 800
   end;
-If internet='S' then
-Monto:= Monto*0.75;
-Writeln('El monto a pagar es ',Monto);
+  end;
+If internet = 'S' then
+	 Monto := Monto * 0.75; //-25% del total
+Writeln('El monto a pagar es $',Monto:4:2);
 readln;
 end.
 
