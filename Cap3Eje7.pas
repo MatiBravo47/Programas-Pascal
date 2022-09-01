2eb539af77b8980f7dd41e857517b7ffbc16a111
MatiBravo47 <Matibravoneron@gmail.com>
2022-09-01 09:31:34 -0300

    add comments

--- Cap3Eje7.pas
+++ Cap3Eje7.pas
@@ -1,27 +1,45 @@
+//Ej 7) Dado un conjunto de alumnos, no se sabe cuántos son, de cada uno de ellos se conoce:
+//- Nombre (‘***’ indica fin de datos)
+//- Nota1, Nota2 y Nota3
+//Ingresar la información e informar:
+//a) El promedio de cada alumno y su condición:
+//− ‘Aprobado’ si su promedio es mayor o igual a 4
+//− ‘Desaprobado’ si su promedio es menor a 4.
+//b) Porcentaje de alumnos Aprobados
+//ejemplo:
+//Enzo
+//4 4 4
+//Matias
+//8 8 8
+//Lauta
+//2 2 2
+//Jorge
+//1 1 1
+//***
 program Cap3Eje7;
 Uses crt;
 Var
-Total,Apro,Nota1,Nota2,Nota3:Byte;
-Arch:Text;
+Total, Apro, Nota1, Nota2, Nota3: Byte;
+Arch: Text;
 Nombre:String[6];
 
 begin
 Clrscr;
-Total:=0;
-Apro:=0;
-Assign(Arch,'Ejercicio37.txt');Reset(Arch);
+Total := 0;
+Apro := 0;
+Assign(Arch,'Ejercicio37.txt'); Reset(Arch);
 Readln(Arch,Nombre);
-While Nombre<>'***' do
+While (Nombre <> '***') do
   Begin
-  Readln(Arch,Nota1,Nota2,Nota3);
-  If ((Nota1+Nota2+Nota3)/3)>=4 then
+  Readln(Arch, Nota1, Nota2, Nota3);
+  If (((Nota1+Nota2+Nota3)/3) >= 4) then
     Begin
     Writeln(Nombre,' Aprobado');
-    Apro:=Apro+1;
+    Apro := Apro + 1;
     end
   else
     Writeln(Nombre,' Desaprobado');
-  Total:=Total+1;
+  Total := Total + 1;
   Readln(Arch,Nombre);
   end;
 Close(Arch);
