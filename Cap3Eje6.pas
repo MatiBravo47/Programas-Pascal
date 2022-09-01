1c41791e0c4072a64a939e222e4cd8469b3053ff
MatiBravo47 <Matibravoneron@gmail.com>
2022-09-01 09:26:11 -0300

    add comments and add close(arch)

--- Cap3Eje6.pas
+++ Cap3Eje6.pas
@@ -1,3 +1,18 @@
+//Ej 6) Se conoce el saldo inicial de una cuenta bancaria y
+//una serie de movimientos realizados, por cada uno:
+//- código de movimiento (D=depósito, R=retiro ó F= fin de datos).
+//- monto
+//Escribir un programa que ingrese dichos datos y determine el saldo
+//exacto de la cuenta después de procesar las transacciones.
+//Al final del proceso indique cuantas veces no pudo retirar dinero por
+//insuficiencia de fondos
+//ejemplo :
+//D 100
+//R 500
+//R 600
+//D 200
+//R 150
+//F
 program Cap3Eje6;
 Uses crt;
 Var
@@ -8,31 +23,32 @@ Mov:Char;
 
 begin
 Clrscr;
-ContR:=0;
-Total:=100;//Monto inicial
-Assign(Arch,'Ejercicio36.txt');Reset(Arch);
-Read(Arch,Mov);
-Mov:=Upcase(Mov);
-While Mov<>'F' do
+ContR := 0;
+Total :=0;
+Assign(Arch,'Ejercicio36.txt'); Reset(Arch);
+Read(Arch, Mov);//lee caracter
+Mov := Upcase(Mov); //pasa a mayuscula
+While (Mov <> 'F') do
 Begin
-Readln(Arch,Saldo);
-If Mov='D'then
-Total:=Total+Saldo
+Readln(Arch, Saldo); //lee saldo
+If (Mov = 'D')then
+	 Total := Total + Saldo //deposito suma
 else
   Begin
   If Mov='R' then
     Begin
-    If Total<Saldo then
-    ContR:=ContR+1
+    If (Total < Saldo) then  //Cuenta intentos fallidos por retirar
+    	 ContR := ContR + 1
     else
-    Total:=Total-saldo;
+    		Total:=Total - saldo; //resta si es posible retirar
     end;
   end;
-Read(Arch,Mov);
-Mov:=Upcase(Mov);
+Read(Arch, Mov);
+Mov := Upcase(Mov);
 end;
-Writeln('No pudo retirar dinero por fondos insuficientes ',ContR,' veces');
-Writeln('El saldo de la cuenta es: ',Total);
+close(arch);
+Writeln('No pudo retirar dinero por fondos insuficientes ', ContR,' veces');
+Writeln('El saldo de la cuenta es: ', Total);
 Readln;
 end.
 
