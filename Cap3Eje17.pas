3f17ef20212d398d2dd577298c4ba1a165d28534
MatiBravo47 <Matibravoneron@gmail.com>
2022-09-03 16:36:26 -0300

    add comments

--- Cap3Eje17.pas
+++ Cap3Eje17.pas
@@ -1,3 +1,17 @@
+//Ej 17) La cajera de un supermercado ha registrado varias ventas,
+//de cada una sabe:
+// Nombre del cliente ( “ ***** “ indica fin de datos )
+// Cantidad de artículos que compró el cliente y por cada uno se conoce:
+//- Precio base del artículo
+//- Si está o no en promoción ( ‘S’ ó ‘N’ )
+//En el caso de estar en promoción, se realiza un descuento del 10%
+//sobre el precio base.
+//Se pide ingresar los datos e informar:
+//a) Monto total de cada venta, el total del descuento y el precio
+//final a pagar.
+//b) Nombre del cliente que compró la mayor cantidad de artículos
+//en oferta.
+//c) Cuántos clientes compraron solo artículos en oferta.
 program Cap3Eje17;
 uses crt;
 var
