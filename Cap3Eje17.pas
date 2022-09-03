6e3ff2296deba294d85fa194435f8d0211b2c5ff
MatiBravo47 <Matibravoneron@gmail.com>
2022-09-03 15:16:01 -0300

    add cap3Eje17

--- Cap3Eje17.pas
+++ Cap3Eje17.pas
@@ -1,44 +1,63 @@
 program Cap3Eje17;
 uses crt;
 var
- Nombre:String[6];
+ Nombre, nombreMayor: String[6];
+ Cant, cantOfMax ,cantOf, CantSoloOfertas: byte;
+ PrecioU, monto, montoT, desc, descT: real;
+ Oferta, mas: char;
+ soloOferta: boolean;
 
 begin
 clrscr;
-Writeln('Ingrese nombre');Readln(Nombre);
-While Nombre<>'***' do
+CantSoloOfertas:= 0;
+CantOfMax:= 0; //Cantidad maxima de compra de articulos en oferta
+Writeln('Ingrese nombre cliente (*** para finalizar)'); Readln(Nombre);
+While (Nombre <> '***') do
   begin
-  CantOfMax:=-3000;
-  CantOf:=0;
-  MontoT:=0;
-  DescT:=0;
-  While Mas='S' do
+  soloOferta:= true;
+  CantOf:= 0;  //cantidad de articulos en oferta comprados
+  MontoT:= 0;
+  DescT:= 0;
+  mas :='S' ; //cargar mas articulos
+  While (Mas = 'S') do //si desea ingresar otro articulo la misma persona
     begin
-    Writeln('Ingrese cantidad del producto');Readln(Cant);
-    Writeln('Ingrese precio unitario');Readln(PrecioU);
-    Writeln('Esta en oferta ? (S/N)');Readln(Oferta);
-    Oferta:=Upcase(oferta);
-    If oferta='S' then
+    Writeln('Ingrese cantidad del producto'); Readln(Cant);
+    Writeln('Ingrese precio del producto'); Readln(PrecioU);
+    Writeln('En oferta ? (S/N)'); Readln(Oferta);
+    Oferta:= Upcase(oferta);
+    If (oferta = 'S') then //si esta en oferta
       begin
-      CantOf:=CantOf+Cant;
-      Desc:=PrecioU*0.90;
-      DescT:=DescT+Desc;
-      Monto:=(Cant*(Desc)
-      MontoT:=MontoT + Monto
-      end;
-    else
+      CantOf:= CantOf + Cant;//cantidad de articulos
+      Desc:= PrecioU * 0.90; //-10% articulo con descuesto
+      DescT:= DescT + Desc;  //total del descuento
+      Monto:= Cant * Desc;   //Monto de cada venta
+      MontoT:= MontoT + Monto; //precio final a pagar
+      end
+    else //si no esta en oferta
       Begin
-      Monto:=Cant*PrecioU
-      MontoT:=MontoT+(Monto);
+      Monto:= Cant * PrecioU; //Monto por mismo articulo
+      MontoT:= MontoT + Monto; //Precio final a pagar
+      soloOferta:= false;
       end;
-    Writeln('Monto venta: ',Monto;)
-    Writeln('Compraste mas productos?(S/N) ' );Readln(Mas);
+    Writeln('Monto venta: $', Monto:5:2);//monto de cada venta
+    Writeln('Compro mas productos?(S/N) ' );Readln(Mas);
     Mas:=Upcase(Mas);
+    if (mas = 'N') then
+      if (cantOf > cantOfMax) then
+        begin
+        cantOfMax:= cantOf;
+        NombreMayor:= nombre;
+        end;
+    	if (soloOferta) then
+        CantSoloOfertas:= CantSoloOfertas + 1;
     end;
-  Writeln(Nombre,' debe abonar',MontoT );
-  Writeln('Descuento total', DescT);
-  Writeln('Ingrese nombre');Readln(Nombre);
+
+  Writeln(Nombre,' debe abonar $', MontoT:5:2 );//precio final a pagar
+  Writeln('Descuento total : $', DescT:5:2); //total del descuento
+  Writeln('Ingrese nombre cliente (*** para finalizar)'); Readln(Nombre);
   end;
+writeln('El cliente que mas cantidad de ofertas compro fue: ',nombreMayor);
+writeln('Compraron solo articulos en oferta ', cantSoloOfertas,' clientes');
 readln;
 end.
 
