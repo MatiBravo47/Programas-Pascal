42ae4161304a6d49879da362a2db3c556cd5cc5c
MatiBravo47 <Matibravoneron@gmail.com>
2022-09-06 09:26:54 -0300

    shorten code

--- Cap4Eje3.pas
+++ Cap4Eje3.pas
@@ -1,23 +1,22 @@
 program Cap4Eje3;
-Function Suma(N:Word):Integer;
+
+Function Suma(N: Word): Integer;
 Var
-  Sum:Integer;
-  Num,i:Word;
+  Sum: Integer;
+  i: Word;
 Begin
-  Sum:=0;
-  Num:=1;
-  For i:=1 To N do
-  Begin
-  Sum:=Sum+(Num);
-  num:=num+1;
-  end;
-Suma:=Sum;
+  Sum:= 0;
+  For i:= 1 To N do
+    Sum:= Sum + i;
+Suma:= Sum; //para evitar estar llamando constante a la funcion
 end;
+
 Var
-  N:Byte;
+  N: Byte;
+
 begin
-  Writeln('Ingrese un numero natural N');Readln(N);
-  Writeln('La suma de los N primeros numeros es ',Suma(N));
+  Writeln('Ingrese un numero natural N'); Readln(N);
+  Writeln('La suma de los N primeros numeros es ', Suma(N));
   Readln;
 end.
 
