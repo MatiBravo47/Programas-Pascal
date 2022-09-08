15bf001ece5f3081774b983fba756d1bf2df2934
MatiBravo47 <Matibravoneron@gmail.com>
2022-09-08 17:38:54 -0300

    add cap4eje13

--- Cap4Eje13.pas
+++ Cap4Eje13.pas
@@ -1,27 +1,43 @@
+//Ej 13) Desarrollar un procedimiento que dada una Hora Inicial
+//(horas, minutos, segundos) y unos segundos transcurridos, devuelva
+//la Hora Final (en horas, minutos y segundos).
 program Cap4Eje13;
 Uses crt;
 Var
-Procedure Horafinal(H,M,S);
+  H, M, S: byte;
+Procedure Horafinal(H, M, S: byte);
+var
+  SegTotal, st: word;
+  horaF, mFinal, sFinal: byte;
 begin
-  Writeln('Ingrese segundos transcurridos');Readln(ST);
-  ST:=S+ST;
-  If ST<=60 then
-    Writeln('La hora final es ',H,':',M,':',ST);
-  else
-    IF ST>
-
-
-
-
+  Writeln('Ingrese segundos transcurridos'); Readln(ST);
+  SegTotal:= S + ST;
+  horaF:= H;
+  mFinal:= M;
+  sFinal:= S;
+  If (segTotal >= 3600 )then //si es mayor a una hora
+     begin
+     horaF:= H + (segTotal div 3600);
+     if (horaF >= 24) then
+        horaF := horaF mod 24;
+     segTotal:= segTotal mod 3600;
+     end;
+  If (segTotal > 60 ) then
+     begin
+     mFinal:= M + (segTotal div 60);
+     segTotal:= segTotal mod 60;
+     end;
+  if (segTotal < 60) then
+     sFinal:= S + segTotal;
+Writeln('La hora final es ',HoraF,':',mFinal,':',sFinal);
 end;
-//Programa ppal
+//PROGRAMA PRINCIPAL
 Begin
 clrscr;
-Writeln('Ingrese hora (HH)');Readln(H);
-Writeln('Ingrese minutos (MM)');Readln(M);
-Writeln('Ingrese segundos (SS)');Readln(S);
-Hora(H,M,S);
+Writeln('Ingrese hora (HH)'); Readln(H);
+Writeln('Ingrese minutos (MM)'); Readln(M);
+Writeln('Ingrese segundos (SS)'); Readln(S);
+HoraFinal(H, M, S);
 Readln;
-
 end.
 
