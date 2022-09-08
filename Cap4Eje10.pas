da0fd454633c54e78b9bf258a00d93000847db22
MatiBravo47 <Matibravoneron@gmail.com>
2022-09-08 10:20:43 -0300

    add exercise 4-10

--- Cap4Eje10.pas
+++ Cap4Eje10.pas
@@ -1,3 +1,5 @@
+//Ej 10) Dado el siguiente trozo de código detectar los errores, enumerarlos
+//y justificar
 program Cap4Eje10;
 ProcedureAAA(var C : char; var N:byte; x,y:real );
 
@@ -9,17 +11,17 @@ Procedure DDD(varN,M:integer);
 
 Var
 Car:char;
-M,Z,W:real;
+M,Z,W:real; //M declarado como integer,
 P:byte;
 N:integer;
 Begin
 Readln(Z,W);
 AAA (‘c’,P,Z,W);
-If CCC(BBB(N,Z<>W),5.4) then
-DDD(N, M)
+If CCC(BBB(N, Z <> W), 5.4) then
+  DDD(N, M)
 Else
-DDD(12.5, N-2);
-Write (BBB(20, P=0));
+DDD(12.5, N - 2); //12.5 no puede ser real,debe ser entero
+Write (BBB(20, P = 0));
 Write (AAA(Car, P, M, Z));
 Readln;
 end.
