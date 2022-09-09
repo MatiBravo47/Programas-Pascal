38e6f64986a6065a27c3c0e63ac4c3dd69826bf9
MatiBravo47 <Matibravoneron@gmail.com>
2022-09-09 17:49:22 -0300

    update exercise 5-2

--- Cap5Eje2.pas
+++ Cap5Eje2.pas
@@ -1,58 +1,62 @@
+//Ej 2) Dado un archivo de enteros, se pide almacenar en un vector V
+//sólo los elementos impares y en otro vector W, los pares, ignorando
+//si hubiese, los ceros.
+//Ejemplo:
+//1 4 3 5 7 4 2 0 98 32
 program Cap5Eje2;
 uses crt;
 Type
-  TVec=Array[1..10] of integer;
-Var
-  N,M,P:Byte;
-  VP,VI,V:TVec;
-Procedure LeeArch(Var N,M:Byte;Var VP,VI:TVec );
-Var
-  arch:Text;
-  Num:Integer;
+  TVec = Array[1..10] of integer;
+var
+  N, M, P: Byte;
+  VP, VI, V: TVec;
 
+Procedure LeeArch(Var N, M: Byte; Var VP, VI: TVec );
+Var
+  arch: Text;
+  Num: Integer;
 Begin
-  N:=0;
-  M:=0;
-  Assign(Arch,'Ejercicio52.txt');Reset(Arch);
+  N:= 0;
+  M:= 0;
+  Assign(Arch,'Ejercicio52.txt'); Reset(Arch);
   While not eof (Arch) do
     Begin
-    Read(Arch,Num);
-    If Num<>0 then
+    Read(Arch, Num);
+    If (Num <> 0) then
       Begin
-      If odd(Num) then
+      If odd(Num) then //Si es impar
         Begin
-        N:=N+1;
-        VI[N]:=Num;
+        N:= N + 1;
+        VI[N]:= Num;
         end
-      else
+      else   //Si es par
         Begin
-        M:=M+1;
-        VP[M]:=Num;
+        M:= M + 1;
+        VP[M]:= Num;
         end;
       end;
     end;
   close(Arch)
 end;
 
-Procedure MuestraVec(P:Byte;V:TVec);
+Procedure MuestraVec(P: Byte ; V: TVec); //Muestra vector
 Var
-  i:Byte;
+  i: Byte;
 Begin
   For i:=1 to P do
-  Begin
-    Write (V[i],' ');
-  end;
+    Begin
+    Write (V[i], ' ');
+    end;
 end;
+
 //Programa principal
 begin
   clrscr;
-  LeeArch(N,M,VP,VI);
+  LeeArch(N, M, VP, VI);
   Writeln('El vector de los numeros impares es');
-  MuestraVec(N,VI);
+  MuestraVec(N, VI);
   Writeln;
   Writeln('El vectores de los numeros pares es');
-  MuestraVec(M,VP);
-
+  MuestraVec(M, VP);
   readln;
 end.
-
