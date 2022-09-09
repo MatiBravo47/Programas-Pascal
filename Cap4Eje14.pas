eed7c613777e720a41614ac8f5a23e070028e8d3
MatiBravo47 <Matibravoneron@gmail.com>
2022-09-09 09:54:03 -0300

    complete exercise

--- Cap4Eje14.pas
+++ Cap4Eje14.pas
@@ -1,60 +1,63 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<CONFIG>
-  <ProjectOptions>
-    <Version Value="11"/>
-    <PathDelim Value="\"/>
-    <General>
-      <Flags>
-        <MainUnitHasCreateFormStatements Value="False"/>
-        <MainUnitHasTitleStatement Value="False"/>
-        <MainUnitHasScaledStatement Value="False"/>
-      </Flags>
-      <SessionStorage Value="InProjectDir"/>
-      <MainUnit Value="0"/>
-      <Title Value="Cap4Eje14"/>
-      <UseAppBundle Value="False"/>
-      <ResourceType Value="res"/>
-    </General>
-    <BuildModes Count="1">
-      <Item1 Name="Default" Default="True"/>
-    </BuildModes>
-    <PublishOptions>
-      <Version Value="2"/>
-      <UseFileFilters Value="True"/>
-    </PublishOptions>
-    <RunParams>
-      <FormatVersion Value="2"/>
-      <Modes Count="0"/>
-    </RunParams>
-    <Units Count="1">
-      <Unit0>
-        <Filename Value="Cap4Eje14.lpr"/>
-        <IsPartOfProject Value="True"/>
-      </Unit0>
-    </Units>
-  </ProjectOptions>
-  <CompilerOptions>
-    <Version Value="11"/>
-    <PathDelim Value="\"/>
-    <Target>
-      <Filename Value="Cap4Eje14"/>
-    </Target>
-    <SearchPaths>
-      <IncludeFiles Value="$(ProjOutDir)"/>
-      <UnitOutputDirectory Value="lib\$(TargetCPU)-$(TargetOS)"/>
-    </SearchPaths>
-  </CompilerOptions>
-  <Debugging>
-    <Exceptions Count="3">
-      <Item1>
-        <Name Value="EAbort"/>
-      </Item1>
-      <Item2>
-        <Name Value="ECodetoolError"/>
-      </Item2>
-      <Item3>
-        <Name Value="EFOpenError"/>
-      </Item3>
-    </Exceptions>
-  </Debugging>
-</CONFIG>
+//Ej 14) Modificar el código del ejemplo 8, dado en la teoría, para que
+//el procedimiento Evalua(), devuelva además, la cantidad de palabras
+//más largas y el programa muestre además, dicho resultado.
+//Considerar que cada palabra finaliza con punto y vienen en líneas
+//diferentes
+program Cap4Eje14;
+Type
+St20= string[20];
+
+Procedure Evalua(Var Todas: string; Var Max, cant: byte);
+Var
+  Arch: Text;
+  Car: char;
+  Pal: St20;
+  i: byte;
+Begin
+  Assign (Arch, 'Ejercicio414.txt'); Reset(Arch);
+  Max:= 0;
+  cant:= 0;
+  Read(Arch, Car);
+  While (Car <> '.') do
+    If (Car = ' ') then
+    Read(Arch, Car) {si es blanco lee otro caracter}
+    else
+     begin {es el comienzo de palabra}
+      Pal:= '';
+      i:= 0;
+      While (Car<>'.') and (Car<>' ') do
+        begin {cuenta los caracteres y arma la palabra}
+        i:= i + 1;
+        Pal:= Pal + Car;
+        Read(Arch, Car);
+        End;
+      If (i > Max) then {evalua si es la mas larga}
+        begin
+        Todas:= Pal;
+        Max:= i;
+        cant:= 1;
+        End
+      else
+        if (i = Max) then {evalua si es igual}
+          begin
+          Todas:= Todas + ' ' + Pal;
+          cant:=cant + 1;
+          end;
+      end;
+Close(Arch);
+End;
+
+Var
+Todas: string;
+Max, cant: byte;
+
+Begin
+Evalua(Todas, max, cant);
+Writeln ('estas son las palabras mas largas') ;
+writeln (Todas);
+Writeln ('Tienen ', Max, ' caracteres') ;
+Writeln('La cantidad de palabras mas largas son :', cant);
+Readln;
+end.
+
+
