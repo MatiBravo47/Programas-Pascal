86707169cb2a75bc5560bec1d4e0cdf09afd1776
MatiBravo47 <Matibravoneron@gmail.com>
2022-09-09 14:41:21 -0300

    add cap4Eje16

---
+++ Cap4Eje16.pas
@@ -0,0 +1,17 @@
+program Cap4Eje16;
+uses crt;
+
+function capicua(num: word):boolean;
+begin
+
+end;
+
+var
+  num:word;
+begin
+  clrscr;
+  writeln('Ingrese un numero'); readln(num);
+  capicua(num);
+  readln;
+end.
+
