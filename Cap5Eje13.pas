0e12fe396bb2ead88e9f96ee5aeb24e12fbd2855
MatiBravo47 <Matibravoneron@gmail.com>
2022-09-11 15:42:42 -0300

    update exercise

--- Cap5Eje13.pas
+++ Cap5Eje13.pas
@@ -3,35 +3,35 @@ Uses crt;
 Type
   TV=Array['A'..'Z'] of byte;
 Var
-  Vec:TV;
-Procedure IniciaVec(Var Vec:TV);
+  Vec: TV;
+
+Procedure IniciaVec(Var Vec: TV);
 Var
-  i:Char;
+  i: Char;
 Begin
   For i:='A' to 'Z' do
-  Vec[i]:=0;
+  Vec[i]:= 0;
 end;
 
-Procedure LeeArch(Var Vec:TV);
+Procedure LeeArch(Var Vec: TV);
 Var
-  Arch:Text;
-  Car:Char;
+  Arch: Text;
+  Car: Char;
 Begin
-  Assign(Arch,'Ejercicio513.txt');Reset(Arch);
+  Assign(Arch,'Ejercicio513.txt'); Reset(Arch);
   While not eof (Arch) do
   Begin
-    Read(Arch,Car);
+    Read(Arch, Car);
     If Upcase(Car) in ['A'..'Z'] then
-    Vec[Upcase(Car)]:=Vec[Upcase(Car)]+1;
+      Vec[Upcase(Car)]:= Vec[Upcase(Car)] + 1;
   end;
-
 end;
-Procedure EscribeVec(Vec:TV);
+Procedure EscribeVec(Vec: TV);
 Var
-  i:Char;
+  i: Char;
 Begin
   For i:='A' to 'Z' do
-  Writeln(i,' : ',Vec[i]);
+    Writeln(i,' : ',Vec[i]);
 end;
 begin
   Clrscr;
