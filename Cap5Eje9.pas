7327b8c273b7a36706f49dfa7ee91ac47a310f0d
MatiBravo47 <Matibravoneron@gmail.com>
2022-09-11 09:24:48 -0300

    update exercise

--- Cap5Eje9.pas
+++ Cap5Eje9.pas
@@ -11,81 +11,84 @@ Uses crt;
 Type
   TVec=array[1..15] of integer;
 Var
-  N, M, O: Byte;
-  V, VA, VB: TVec;
-Procedure VectorA(Var N: Byte;Var VA: Tvec);
+  N: Byte;
+  VA, VB: TVec;
+
+Procedure VectorA(Var N: Byte;Var VA: Tvec); //Almacena ascendentes
 Var
   Arch: Text;
-  Max, Num: Integer;
+  ant, Num: Integer;
 Begin
   Assign(Arch, 'Ejercicio59.txt'); Reset(Arch);
-  Read(Arch, Max);
-  VA[1]:= Max;
+  Read(Arch, ant);
   N:= 1;
+  VA[N]:= ant; //Asigno el primer valor como maximo
   While not eof (arch) do
   Begin
-    Read(Arch,Num);
-    If Num>Max then
-    Begin
-    Max:=Num;
-    N:=N+1;
-    VA[N]:=Num;
+    Read(Arch, Num);
+    If (Num > ant) then //si es mayor al anterior
+      Begin
+      ant:= Num;
+      N:= N + 1;
+      VA[N]:= Num;
     end;
   end;
   Close(Arch);
 end;
+
 Procedure MuestraVector(O:Byte;V:Tvec);
 Var
-  i:Byte;
+  i: Byte;
 Begin
   For i:=1 to O do
-  Write(' ',V[i])
+  Write(' ', V[i])
 end;
-Procedure VectorB(N:Byte;VA:TVec;Var VB:TVec);
+
+Procedure VectorB(N: Byte; VA: TVec; Var VB: TVec);
 Var
-  i,j:Byte;
+  i, j: Byte;
 Begin
-  i:=1;
-  j:=N;
-  While i<>j do
+  i:= 1;
+  j:= N;
+  While (i <> j) do
   Begin
-    If VA[i]>VA[J] then
-    Begin
-      If (VA[i])mod(VA[j])=0 then
+    If (VA[i] > VA[J]) then
       Begin
-      VB[i]:=VA[i];
-      VB[J]:=Va[j];
-      end
-    end
-    Else
-    begin
-      If VA[i]<VA[J] then
-      Begin
-        If (VA[J])mod(VA[I])=0 then
+      If ((VA[i])mod(VA[j]) = 0) then
         Begin
-        VB[i]:=VA[i];
-        VB[J]:=Va[j];
-        end;
-      end;
+        VB[i]:= VA[i];
+        VB[J]:= Va[j];
+        end
+      end
+    Else
+      begin
+      If (VA[i] < VA[J]) then
+        Begin
+        If ((VA[J]) mod (VA[I]) = 0) then
+          Begin
+          VB[i]:= VA[i];
+          VB[J]:= Va[j];
+          end;
+         end;
+       end;
+    i:= i + 1;
+    J:= J - 1;
     end;
-    i:=i+1;
-    J:=J-1;
-    end;
-  If i=j then
-  VB[i]:=VA[i];
+  If (i = j) then
+    VB[i]:= VA[i];
 end;
 
 
 //Programa principal
 begin
   clrscr;
-  VectorA(N,VA);
+  VectorA(N, VA);
   Writeln('El vector A es');
-  MuestraVector(N,VA);
-  VectorB(N,VA,VB);
+  MuestraVector(N, VA);
+  VectorB(N, VA, VB);
   Writeln;
   Writeln('El vector B es');
-  MuestraVector(N,VB);
+  MuestraVector(N, VB);
   readln;
 end.
 
