0609d9f5d885bb13b1dc1d26e412927d538a1804
MatiBravo47 <Matibravoneron@gmail.com>
2022-09-12 13:16:15 -0300

    update exercise

--- Cap5Eje14.pas
+++ Cap5Eje14.pas
@@ -1,89 +1,100 @@
+//En una estación de peaje hay N cabinas por donde pasan varios
+//vehículos. Desde un archivo se lee
+//la siguiente información:
+//• Número de cabina
+//• Tipo de vehículo (1=auto, 2=moto, 3=camioneta, 4=camión, 5=colectivo)
+//• Hora (0..23)
+//Las tarifas son: moto = $150, auto y camioneta = $300, camión = $470,
+//colectivo = $390.
+//Si la hora está entre las 10 y las 18 sufre un incremento del 10%.
+//Calcular e informar:
+//a) Cuántos vehículos atendió cada una de las N cabinas.
+//b) Cuál fue la que más importe recaudó
 program Cap5Eje14;
 Uses crt;
 Type
   TVByte=Array[1..10] Of Byte;
   TVReal=Array[1..10] Of Real;
-
-
 Var
-  N:Byte;
-  VV:TVByte;
-  VR:TVReal;
-Procedure IniciaVec(N:Byte;Var VR:TVReal);
+  N: Byte;
+  VV: TVByte;
+  VR: TVReal;
+
+Procedure IniciaVec(N: Byte; Var VR: TVReal);
 Var
-i:Byte;
+  i: Byte;
 Begin
 For i:=1 to N do
-VR[i]:=0;
+  VR[i]:=0;
 end;
-Procedure IniciaVec(N:Byte;Var VV:TVByte);
+Procedure IniciaVec(N: Byte; Var VV: TVByte);
 Var
-i:Byte;
+  i:Byte;
 Begin
 For i:=1 to N do
-VV[i]:=0;
+  VV[i]:= 0;
 end;
 
-Procedure LeeArch(Var VR:TVReal;Var VV:TVByte);
+Procedure LeeArch(Var VR: TVReal; Var VV: TVByte);
 Var
-  Arch:Text;
-  Cab,Vehi,Hora:Byte;
-  Tarifa:Word;
-  Monto:Real;
+  Arch: Text;
+  Cab, Vehi, Hora: Byte;
+  Tarifa: Word;
+  Monto: Real;
 Begin
-  Assign(Arch,'Ejercicio514.txt');Reset(Arch);
+  Assign(Arch,'Ejercicio514.txt'); Reset(Arch);
   While not eof (Arch) do
   Begin
-    Monto:=0;
-    Readln(Arch,Cab,Vehi,hora);
-    VV[Cab]:=VV[Cab]+1;
+    Monto:= 0;
+    Readln(Arch, Cab, Vehi, hora);
+    VV[Cab]:= VV[Cab] + 1;
     Case Vehi of
-    1,3:Tarifa:=30;
-    2:Tarifa:=15;
-    4:Tarifa:=70;
-    5:Tarifa:=90;
+    1, 3: Tarifa:= 30;
+    2: Tarifa:= 15;
+    4: Tarifa:= 70;
+    5: Tarifa:= 90;
     end;
     If (hora in [10..18 ]) then
-    Monto:=Monto+(Tarifa*1.10)
+      Monto:= Monto + (Tarifa * 1.10)
     else
-    Monto:=Monto+Tarifa;
-    VR[Cab]:=VR[Cab]+Monto;
+      Monto:= Monto + Tarifa;
+    VR[Cab]:= VR[Cab] + Monto;
   end;
 end;
 
-Procedure Vehiculos(VV:TVByte;N:Byte);
+Procedure Vehiculos(VV: TVByte; N: Byte);
 Var
   i:Byte;
 Begin
   For i:=1 to N do
   Begin
-    Writeln('La cabina',i, ' atendio ',VV[i],' vehiculos' );
+    Writeln('La cabina ',i, ' atendio ', VV[i],' vehiculos' );
   end;
 end;
 
-Function MasImporte(VR:TVReal;N:byte):Byte;
+Function MasImporte(VR: TVReal; N: byte): Byte;
 Var
-  i,IMax:Byte;
-  Mas:Real;
+  i, IMax: Byte;
+  Mas: Real;
 Begin
-  Mas:=VR[1];
+  Mas:= VR[1];
   For i:=2 to N do
   Begin
-    If VR[i]>Mas then
-    Begin
-    Mas:=VR[i];
-    IMax:=i;
+    If (VR[i] > Mas) then
+      Begin
+      Mas:= VR[i];
+      IMax:= i;
     end;
   end;
-  MasImporte:=Imax;
+  MasImporte:= Imax;
 end;
 begin
   Clrscr;
-  Writeln('Ingrese cantidad de cabinas');Readln(N);
-  IniciaVec(N,VV);
-  IniciaVec(N,VR);
-  LeeArch(VR,VV);
-  Vehiculos(VV,N);
+  Writeln('Ingrese cantidad de cabinas'); Readln(N);
+  IniciaVec(N, VV);
+  IniciaVec(N, VR);
+  LeeArch(VR, VV);
+  Vehiculos(VV, N);
   Writeln('La cabina que mas recaudo fue la ', MasImporte(VR,N));
   Readln;
 end.
