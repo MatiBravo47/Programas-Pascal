2e966bde2de80d63909a0db368c845c94727e505
MatiBravo47 <Matibravoneron@gmail.com>
2022-10-06 14:57:29 -0300

    add comments

--- Capitulo7/Cap7Eje2.pas
+++ Capitulo7/Cap7Eje2.pas
@@ -1,3 +1,13 @@
+//Ej 2) En una matriz Tablero de 8x8 se almacenó una jugada de
+//ajedrez. Además, se sabe la posición (i,j) de una de las
+//Torres Negras, y que juegan las negras. Determinar e informar
+//a qué piezas “defiende” la Torre y a cuáles “amenaza”,
+//sabiendo que las piezas tienen dos atributos:
+// Tipo: R = Rey, Q = Reina, A = Alfil, T = Torre,
+//C = Caballo, P = Peón
+// Color: 1 = Blanco, 2 = Negro
+//Nota: armar el Tablero leyendo desde archivo, donde en
+//cada línea viene (fila, col, Tipo, Color)
 program Cap7Eje2;
 Uses crt;
 
