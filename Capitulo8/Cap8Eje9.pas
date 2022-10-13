cfda6d721314be2e6ae6296ebfbd95e1fb3a9264
MatiBravo47 <Matibravoneron@gmail.com>
2022-10-13 12:13:20 -0300

    add exercise

---
+++ Capitulo8/Cap8Eje9.pas
@@ -0,0 +1,31 @@
+program Cap8Eje9;
+function esCreciente(num: word): boolean;
+var
+   d1, d2: byte;
+begin
+If (num div 10 = 0) then
+   esCreciente:= true
+else
+   begin
+   d1:= (num div 10) mod 10;//Anteultimo digito
+   d2:= (num mod 10); //Ultimo digito
+   If (d2 >= d1) then
+      esCreciente:= esCreciente(num div 10)
+   else
+      esCreciente:= false;
+   end;
+end;
+
+//Programa Principal//
+var
+   num: word;
+
+begin
+Writeln('Ingrese un numero'); Readln(num);
+If esCreciente(num) then
+   Writeln('Es creciente')
+else
+   Writeln('No es creciente');
+Readln;
+end.
+
