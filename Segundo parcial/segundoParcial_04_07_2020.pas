ada6389330a3f562dede8ade7d8073696a371670
MatiBravo47 <Matibravoneron@gmail.com>
2022-10-19 22:59:46 -0300

    add parcial_04_07_20

---
+++ Segundo parcial/segundoParcial_04_07_2020.pas
@@ -0,0 +1,38 @@
+//Una casa de comidas rápidas reabrió sus puertas el
+//1ero de junio (después de la cuarentena) realizando
+//la venta de 3 promociones sólo por delivery, con los
+//siguientes costos: Promo 1=$700, Promo 2=$400 y Promo 3=$500.
+//Realiza entregas en dos turnos (Mediodía y Noche) y a la noche
+//los precios se incrementan en un 10%
+//Para decidir si continuar con la actividad, se desea analizar la venta de
+//todo el mes de junio, para lo cual se dispone de un archivo de texto
+//(ordenado por día y turno) que contiene en cada línea los datos de un turno:
+//DIA (1 a 30) - TURNO (M/N) - CANTIDAD PROMOS VENDIDA (1 a 100) –
+//DETALLE PROMOS VENDIDAS (nros de promo
+//en secuencia).
+//Con esa información, generar las estructuras necesarias para indicar:
+//Dia, Turno, CantPromo1, CantPromo2 y CantPromo3 y luego:
+//a) Dado un día ingresado por teclado, mostrar la recaudación total
+//incluyendo ambos turnos (tener en cuenta que el
+//día puede no existir o no tener ventas en algún turno)
+//b) Mostrar la recaudación total de cada promoción
+//c) Mostrar las estructuras generadas
+//NOTA: Para aprobar debe resolver correctamente en forma recursiva
+//los siguientes procesos:
+//- Mostrar la Matriz óInicializar la Matriz
+//- Obtener la recaudación de un determinado turno de determinado día
+//Importante:
+//• Nombrar su código y el archivo .PAS con su apellido.
+//• La solución se debe implementar usando indefectiblemente registros y
+//arreglos (vectores y matrices)
+//• Se considerarán la eficiencia, claridad y modularización adecuada del código.
+//Los datos del ejemplo son ilustrativos para realizar una ejecución con
+//los mismos, NO deben figurar como constantes en el código desarrollado.
+program segundoParcial_04_07_2020;
+uses crt;
+
+begin
+clrscr;
+readln;
+end.
+
