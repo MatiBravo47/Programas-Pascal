3a38190ea807ce65463f6e6a92c239dc309c240e
MatiBravo47 <matibravoneron@gmail.com>
2022-11-06 00:39:35 -0300

    improve code

--- Segundo parcial/backup/segundoParcial_28_10_19.pas
+++ Segundo parcial/backup/segundoParcial_28_10_19.pas
@@ -15,8 +15,6 @@
 //ganancia.
 //c) Para un importe X, cuantos rubros registraron ventas por encima de X en
 // todas las cajas.
-
-
 program segundoParcial_28_10_19;
 uses crt;
 type
@@ -25,139 +23,145 @@ type
     rubro:ST;
     margen:real;
   end;
+
   TVR = array[1..10] of Treg;
   TM = array[1..50,1..50] of real;
 
-procedure blanqueo(var Mat:TM;i,j,m:byte);
+procedure blanqueo(var Mat: TM; i, j, m: byte);
 begin
   if (i >= 1 ) then
     begin
     if (j > 1 ) then
-      blanqueo(Mat, i, j-1, M)
+      blanqueo(Mat, i, j - 1, M)
     else
-      blanqueo(Mat,i-1,M,M);
+      blanqueo(Mat, i - 1, M, M);
     Mat[i,j]:= 0;
     end;
 end;
 
-procedure lectura(var cajaRubro:TM; var N, M: byte; var productos:TVR);
+procedure lectura(var cajaRubro: TM; var N, M: byte; var productos: TVR);
 var
-  arch:text;
-  i,j:byte;
-  val:integer;
+  arch: text;
+  i, j: byte;
+  val: integer;
 begin
-  assign(arch, 'parcial2.txt');reset(arch);
-  readln(arch,n,m);
-  blanqueo(cajaRubro,n,m,m);
-  for j:=1 to M do
+  assign(arch, 'parcial2.txt'); reset(arch);
+  readln(arch, n, m);
+  blanqueo(cajaRubro, n, m, m);
+  for j:= 1 to M do
     begin
     with productos[j] do
-      readln(arch,rubro,margen)
+      readln(arch, rubro, margen)
     end;
-  for i:=1 to N do
+  for i:= 1 to N do
     begin
-    read(arch,j,val);
-    while (j<>0) and (val <> 0) do
+    read(arch, j, val);
+    while (j <> 0) and (val <> 0) do
       begin
-      cajaRubro[i,j]:=cajaRubro[i,j] + val;
-      read(arch,j,val)
+      cajaRubro[i,j]:= cajaRubro[i,j] + val;
+      read(arch, j, val)
       end;
     readln(arch);
     end;
   close(arch);
 end;
 
-function buscaRubro(R:ST;productos:TVR;M:byte):byte;
+function buscaRubro(R: ST; productos: TVR; M: byte): byte;
   begin
     if (m = 0) then
       buscaRubro :=0
     else
-      if productos[m].rubro=r then
-        buscaRubro:=m
+      if (productos[m].rubro = r) then
+        buscaRubro:= m
       else
-        buscaRubro:=buscaRubro(r,productos,m-1);
+        buscaRubro:= buscaRubro(r, productos, m - 1);
     end;
 
-function promedio(cajaRubro:TM;n,m:byte):integer;
+function promedio(cajaRubro: TM; n, m: byte): real;
 var
-  suma :integer;
+  suma: real;
 begin
-  if n<1 then
-    suma:=0
+  if (n < 1) then
+    suma:= 0
   else
     begin
-    promedio:= suma + promedio(cajaRubro,n-1,m);
+    promedio:= suma + promedio(cajaRubro, n - 1, m);
     suma:= cajaRubro[n,m];
     end;
-  promedio:=suma;
+  promedio:= suma;
 end;
 
-procedure maxGan(cajaRubro:TM; productos:TVR;i,m;var aux:byte; var val:real);
+procedure maxGan(cajaRubro: TM; productos: TVR; i, m: byte; var aux: byte; var val: real);
 begin
-  if M=1 then
+  if (M = 1) then
     begin
-    aux:=1
-    val:= cajaRubro[i,j]*(productos[m].margen)/100;
-    end;
+    aux:= 1;
+    val:= cajaRubro[i,1] * (productos[m].margen) / 100;
+    end
   else
-    maxGan(cajaRubro, productos, i , m-1,aux,val);
-  if (cajaRubro[i,m]*(productos[m].margen)/100)> val then
+    maxGan(cajaRubro, productos, i , m - 1, aux, val);
+  if ((cajaRubro[i,m] * (productos[m].margen)/100) > val) then
     begin
-    val:= (cajaRubro[i,m]*productos[m].margen)/100);
-    aux:=m;
+    val:= (cajaRubro[i,m] * productos[m].margen) / 100;
+    aux:= m;
     end;
-end
+end;
 
-function controlXCaja(cajaRubro:TM;N,M:byte;x:integer):boolean;
+function controlXCaja(cajaRubro: TM; N, M: byte; x: integer): boolean;
 begin
-  if n=0 then
+  if (n = 0) then
     controlXCaja:= true
   else
     begin
-    if cajaRubro[n,m]<x then
-      controlXCaja:=false
+    if (cajaRubro[n,m] < x) then
+      controlXCaja:= false
     else
-      controlXCaja:= controlXCaja(cajaRubro,n-1,m,x);
+      controlXCaja:= controlXCaja(cajaRubro, n - 1, m, x);
     end;
 end;
 
-function cuentaRubrosX(cajaRubro:TM;N,M:byte;x:integer):byte;
+function cuentaRubrosX(cajaRubro: TM; N, M: byte; x: integer): byte;
 var
-  incr:byte;
+  incr: byte;
 begin
-  if M<1 then
-    cuentraRubroX:=0
+  if (M < 1) then
+    cuentaRubrosX:= 0
   else
     begin
-    if controlXCaja(cajaRubro,n,m,x) then
-      incr:=1
+    if controlXCaja(cajaRubro, n, m, x) then
+      incr:= 1
     else
-      incr:=0;
-    cuentraRubros:= incr + cuentaRubros(cajaRubro,n,m-1,x);
+      incr:= 0;
+    cuentaRubrosX:= incr + cuentaRubrosX(cajaRubro, n, m - 1, x);
     end;
 end;
 
 //programa principal
 var
-  productos:TVR;
-  cajaRubro:TM;
-  N,M,auxRubro,auxRubroMax,D:byte;
-  R:ST;
-  X:integer;
-  valMaxRubro:real;
+  productos: TVR;
+  cajaRubro: TM;
+  N, M, auxRubro, auxRubroMax, D: byte;
+  R: ST;
+  X: integer;
+  valMaxRubro: real;
 
 begin
-  lectura(cajaRUbro,n,m,productos);
+  lectura(cajaRUbro, n, m, productos);
   writeln('Ingrese el rubro(8 caracteres complete con espacio)' );
   readln(r);
-  auxRubro:= buscaRubro(r,productos,m);
-  if auxRubro=0 then
+  auxRubro:= buscaRubro(r, productos, m);
+  if (auxRubro = 0) then
     writeln('rubro no encontrado')
   else
-    writeln('para el rubro ,',r,'su promedio de ventas es de ',
-    promedio(cajaRubro,n,m)/N:5:2);
+    writeln('para el rubro ,', r ,'su promedio de ventas es de ',
+    promedio(cajaRubro, n, m) / N:5:2);
   writeln('ingrese la caja D');
   readln(D);
-
+  maxGan(cajaRubro, productos, d, m, auxRubroMax, valMaxRubro);
+  writeln('para la caja', d,'el rubro con mayor ganancia es el ', auxRubroMax ,
+  '(' , productos[auxRubroMax].rubro,') con ' , valMaxRubro:5:2, '$');
+  readln(x);
+  writeln(' para x=', x,' ', cuentaRubrosX(cajaRubro, n, m, x),'rubros');
+  readln()
 end.
 
